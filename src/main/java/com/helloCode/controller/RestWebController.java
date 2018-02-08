1c451e0870cbef4c6cc8800b76902d27958f97be
ryhan000 <rayhan1ahamed@gmail.com>
2018-02-08 14:35:04 +0600

    integrate JQuery Ajax POST/GET & Spring MVC

---
+++ src/main/java/com/helloCode/controller/RestWebController.java
@@ -0,0 +1,35 @@
+package com.helloCode.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.helloCode.message.Response;
+import com.helloCode.model.Customer;
+
+@RestController
+@RequestMapping("/api/customer")
+public class RestWebController {
+ 
+	List<Customer> cust = new ArrayList<Customer>();
+ 
+	@GetMapping(value = "/all")
+	public Response getResource() {
+		Response response = new Response("Done", cust);
+		return response;
+	}
+ 
+	@PostMapping(value = "/save")
+	public Response postCustomer(@RequestBody Customer customer) {
+		cust.add(customer);
+		
+		// Create Response Object
+		Response response = new Response("Done", customer);
+		return response;
+	}
+}
